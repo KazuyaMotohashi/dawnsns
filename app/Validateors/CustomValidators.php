896376ee34c46ed366781cfa40d8984cbdd59b8b
KazuyaMotohashi <110092679+KazuyaMotohashi@users.noreply.github.com>
2022-08-02 16:36:37 +0900

    no message

---
+++ app/Validateors/CustomValidators.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\Validators;
+
+use Illuminate\Validation\Validator;
+
+class CustomValidator extends Validator
+{
+    /**
+     * alpah
+     *
+     * @param string $attribute
+     * @param string $value
+     * @return true
+     */
+    public function validateAlpha($attribute, $value)
+    {
+        return (preg_match("/^[a-z]+$/i", $value));
+    }
+
+    /**
+     * alpah_dash
+     *
+     * @param string $attribute
+     * @param string $value
+     * @return true
+     */
+    public function validateAlphaDash($attribute, $value)
+    {
+        return (preg_match("/^[a-z0-9_-]+$/i", $value));
+    }
+
+    /**
+     * alpah_num
+     *
+     * @param string $attribute
+     * @param string $value
+     * @return true
+     */
+    public function validateAlphaNum($attribute, $value)
+    {
+        return (preg_match("/^[a-z0-9]+$/i", $value));
+    }
+}
